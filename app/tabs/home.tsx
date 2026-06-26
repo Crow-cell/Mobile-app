21ff466d325598be1ab1405afd7ee91f4bf4ffa2
Crow-cell <miezmarcel302@gmail.com>
2026-06-26 21:20:19 +0300

    Am adaugat weekly tracker

--- app/tabs/home.tsx
+++ app/tabs/home.tsx
@@ -1,7 +1,15 @@
 import {StyleSheet, Text, View,Image,Pressable} from "react-native";
 import { Link } from 'expo-router';
-import {Ionicons} from '@expo/vector-icons'
+import {Ionicons} from '@expo/vector-icons';
+import {useState} from 'react';
+
+
+
 export default function Index() {
+    const[Luni,setLuni]=useState(false)
+    const[Marti,setMarti]=useState(false)
+    const[Miercuri,setMiercuri]=useState(false)
+    const[Joi,setJoi]=useState(false)
   return (
     <View>
     <View style={styles.header}>
@@ -22,12 +30,30 @@ export default function Index() {
      </Pressable>
      </Link>
    </View>
-   <View style={styles.body}>
-
+   <View style={styles.body}>{ /* body-ul dintre nav bar de sus si tab bar */} 
+     <View style={styles.trackbar}><Text style={styles.contentTrackbar}>WEEKLY TRACKER</Text></View>
+     <View> // bara cu literele cu zile
+      <Text style={styles.dayBar}>M     T      W      T      F      S       S</Text>
+      </View>
+     <View style={styles.baraCercuri}>{/* bara cu progresul*/}
+     <Pressable style={styles.cerc} onPress={()=>setLuni(!Luni)}>
+      {Luni ? (
+         <Ionicons name="checkmark-circle" size={30} color="limegreen"/>  /* if true*/
+      ): (
+        <Ionicons name="ellipse-outline" size={30} color="white"/>      /* if false*/
+      )}
+      </Pressable>
+      <Pressable style={styles.cerc} onPress={()=>setMarti(!Marti)}>
+        {Marti ? (
+            <Ionicons name="checkmark-circle" size={30} color="limegreen" />
+          ): (
+            <Ionicons name="ellipse-outline" size={30} color="white"/>
+          )}
+        </Pressable>     
+     </View>
    </View>
-
    </View>
-  );
+  )
 }
 const styles=StyleSheet.create({
    header:{
@@ -40,11 +66,38 @@ const styles=StyleSheet.create({
     paddingTop:30,
   },
    body:{
-
+     backgroundColor:"#121212",
+     width:"100%",
+     height:800,
+     flexDirection:"column",
    },
-   content:{
-   fontSize:30,
+   contentTrackbar:{
+   fontSize:25,
    color:"white",
    fontWeight:"bold",  
-   }
+   },
+   trackbar:{
+    width:"100%",
+    backgroundColor:"rgb(33,115,127)",
+    height:40,
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  dayBar:{
+    color:"white",
+    fontSize:25,
+    marginLeft:30,
+  },
+  baraCercuri:{
+    width:"100%",
+    height:10,
+    backgroundColor:"#121212",
+    gap:50,
+    flexDirection:"row",
+  },
+  cerc:{
+    width:30,
+    height:100,
+    borderRadius:"50%",
+  }
 })
